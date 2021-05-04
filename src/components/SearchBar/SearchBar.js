94576c17cdbc157e4e43de9b09378c506edc2a56
masculus <magnus.masculus971114@gmail.com>
2021-05-04 14:08:15 +1000

    add clearable button to dropdowns

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,27 +1,15 @@
 import { React, useState, useEffect } from "react";
-import {
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  InputGroup,
-  InputGroupAddon,
-  Input,
-  Button,
-  Form,
-} from "reactstrap";
 import Select from "react-select";
 import "./SearchBar.css";
 
 function SearchBar(props) {
-  // const [innerSearch, setInnerSearch] = useState("");
-  // const [countryDropdownOpen, setCountryDropdownOpen] = useState(false);
-  // const [yearsDropdownOpen, setYearsDropdownOpen] = useState(false);
   const [selectedCountry, setSelectedCountry] = useState(
     `${props.currentCountry}`
   );
   const [selectedYear, setSelectedYear] = useState(`${props.currentYear}`);
   const [countryNames, setCountryNames] = useState([]);
+  const [isClearableCountry, setIsClearableCountry] = useState(false);
+  const [isClearableYear, setIsClearableYear] = useState(false);
 
   let yearsOptions = [
     { label: "2020", value: "2020" },
@@ -51,6 +39,14 @@ function SearchBar(props) {
     yearsOptions.unshift({ label: "All", value: "All" });
   }
 
+  function toggleClearableCountry() {
+    setIsClearableCountry(!isClearableCountry);
+  }
+
+  function toggleClearableYear() {
+    setIsClearableYear(!isClearableYear);
+  }
+
   return (
     <div className="search-bar">
       <div className="row my-4">
@@ -64,13 +60,24 @@ function SearchBar(props) {
               value={countryNames.filter(
                 (option) => option.label === selectedCountry
               )}
+              // defaultValue={countryNames.filter(
+              //   (option) => option.label === selectedCountry
+              // )}
+              isClearable={isClearableCountry}
               onChange={(e) => {
-                if (e.value === "All") {
+                if (e !== null) {
+                  if (e.value === "All") {
+                    props.onSubmitCountry("");
+                    setSelectedCountry("All");
+                  } else {
+                    props.onSubmitCountry(e.value);
+                    setSelectedCountry(e.value);
+                    toggleClearableCountry();
+                  }
+                } else {
                   props.onSubmitCountry("");
                   setSelectedCountry("All");
-                } else {
-                  props.onSubmitCountry(e.value);
-                  setSelectedCountry(e.value);
+                  toggleClearableCountry();
                 }
               }}
             />
@@ -83,68 +90,27 @@ function SearchBar(props) {
               value={yearsOptions.filter(
                 (option) => option.label === selectedYear
               )}
+              isClearable={isClearableYear}
               onChange={(e) => {
-                if (e.value === "All") {
-                  props.onSubmitYear("");
-                  setSelectedYear("All");
+                if (e !== null) {
+                  if (e.value === "All") {
+                    props.onSubmitYear("");
+                    setSelectedYear("All");
+                    toggleClearableYear();
+                  } else {
+                    props.onSubmitYear(e.value);
+                    setSelectedYear(e.value);
+                    toggleClearableYear();
+                  }
                 } else {
-                  props.onSubmitYear(e.value);
-                  setSelectedYear(e.value);
+                  props.onSubmitYear("2020");
+                  setSelectedYear("2020");
+                  toggleClearableYear();
                 }
               }}
             />
           </div>
         </div>
-        {/* <div className="col-12 col-md-5 ">
-          <Form
-            className="mt-3"
-            onSubmit={(e) => {
-              e.preventDefault();
-              if (innerSearch === "") {
-                setSelectedYear("2020");
-                setSelectedCountry("All");
-                props.onSubmitText(innerSearch);
-                props.onSubmitYear("2020");
-              } else {
-                setSelectedYear(props.currentYear);
-                setSelectedCountry(innerSearch);
-                props.onSubmitText(innerSearch);
-                props.onSubmitYear(props.currentYear);
-              }
-            }}
-          >
-            <InputGroup>
-              <Input
-                placeholder="Search country name..."
-                id="search"
-                value={innerSearch}
-                onChange={(e) => setInnerSearch(e.target.value)}
-                arialabelledby="search-button"
-              />
-              <InputGroupAddon addonType="append">
-                <Button
-                  id="search-button"
-                  type="button"
-                  onClick={() => {
-                    if (innerSearch === "") {
-                      setSelectedYear("2020");
-                      setSelectedCountry("All");
-                      props.onSubmitText(innerSearch);
-                      props.onSubmitYear("2020");
-                    } else {
-                      setSelectedYear(props.currentYear);
-                      setSelectedCountry(innerSearch);
-                      props.onSubmitText(innerSearch);
-                      props.onSubmitYear(props.currentYear);
-                    }
-                  }}
-                >
-                  Search
-                </Button>
-              </InputGroupAddon>
-            </InputGroup>
-          </Form>
-        </div> */}
       </div>
     </div>
   );

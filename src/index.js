29c9f584811c06ce96d916c647b5655c1ab177ea
masculus <magnus.masculus971114@gmail.com>
2021-05-10 19:16:32 +1000

    Remove strict mode

--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));

0135775b3f5127ce672596b1b51715523a6579df
masculus <magnus.masculus971114@gmail.com>
2021-07-06 11:41:22 +1000

    Update build

--- src/pages/Factors.js
+++ src/pages/Factors.js
@@ -9,7 +9,7 @@ import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 function Factors() {
   const openWikiPage = () =>
     (window.location.href =
-      "https://en.wikipedia.org/wiki/World_Happiness_Report/");
+      "https://en.wikipedia.org/wiki/World_Happiness_Report");
   return (
     <div>
       <Jumbotron>
